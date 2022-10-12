0304837d1d58a021e4a531e648442cec7022ef7d
jimmy <toluadejimi@gmail.com>
2022-10-12 02:21:46 +0100

    up

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -92,12 +92,12 @@
                                             <label>Password</label>
                                             <div class="mb-3">
                                                 <input type="password" name="password" class="form-control"
-                                                    placeholder="Password" aria-label="Password"
+                                                    placeholder="Password" aria-label=""
                                                     aria-describedby="password-addon">
                                             </div>
                                             <div class="form-check form-switch">
                                                 <input class="form-check-input" type="checkbox" id="rememberMe"
-                                                    checked="">
+                                                    checked=" ">
                                                 <label class="form-check-label" for="rememberMe">Remember me</label>
                                             </div>
                                             <div class="text-center">
