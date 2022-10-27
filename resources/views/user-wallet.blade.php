65e2aefde5765daf02f1e3dff1ace013b93ff113
jimmy <toluadejimi@gmail.com>
2022-10-27 20:31:27 +0100

    cv

--- resources/views/user-wallet.blade.php
+++ resources/views/user-wallet.blade.php
@@ -55,7 +55,7 @@
 
                             @foreach ($wallet as $item)
                                 <tr>
-                                    <td>{{ $item->user_id->user->l_name }}</td>
+                                    <td>{{ $item->user_id }}</td>
                                     <td>{{ $item->current_balance }}</td>
 
                                     <td>
